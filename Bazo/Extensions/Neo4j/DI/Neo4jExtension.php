9ee5dcd4b29dcb43f5cfd8ea69750fd8d49c1027
Martin Bazik <martin@bazo.sk>
2013-05-14 21:25:33 +0200

    update to newest nette master

--- Bazo/Extensions/Neo4j/DI/Neo4jExtension.php
+++ Bazo/Extensions/Neo4j/DI/Neo4jExtension.php
@@ -1,19 +1,18 @@
 <?php
+
 namespace Bazo\Extensions\Neo4j\DI;
 
-use Doctrine\Common\ClassLoader,
-	Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationReader;
 
 /**
  * Description of Neo4jExtension
  *
  * @author Martin Bažík
  */
-class Neo4jExtension extends \Nette\Config\CompilerExtension
+class Neo4jExtension extends \Nette\DI\CompilerExtension
 {
-	/**
-	 * @var array
-	 */
+
+	/** @var array */
 	public $defaults = array(
 		'host' => 'localhost',
 		'port' => 7474,
@@ -25,17 +24,16 @@ class Neo4jExtension extends \Nette\Config\CompilerExtension
 		'password' => null
 	);
 	
-	private static
-		$cacheClassMap = array(
-			'array' => '\Doctrine\Common\Cache\ArrayCache',
-			'apc' => '\Doctrine\Common\Cache\ApcCache',
-			'filesystem' => '\Doctrine\Common\Cache\FilesystemCache',
-			'phpFile' => '\Doctrine\Common\Cache\PhpFileCache',
-			'winCache' => '\Doctrine\Common\Cache\WinCacheCache',
-			'xcache' => '\Doctrine\Common\Cache\XcacheCache',
-			'zendData' => '\Doctrine\Common\Cache\ZendDataCache'
-		)
-	;		
+	/** @var array */
+	private static $cacheClassMap = array(
+		'array' => '\Doctrine\Common\Cache\ArrayCache',
+		'apc' => '\Doctrine\Common\Cache\ApcCache',
+		'filesystem' => '\Doctrine\Common\Cache\FilesystemCache',
+		'phpFile' => '\Doctrine\Common\Cache\PhpFileCache',
+		'winCache' => '\Doctrine\Common\Cache\WinCacheCache',
+		'xcache' => '\Doctrine\Common\Cache\XcacheCache',
+		'zendData' => '\Doctrine\Common\Cache\ZendDataCache'
+	);
 
 	/**
 	 * Processes configuration data
@@ -45,32 +43,32 @@ class Neo4jExtension extends \Nette\Config\CompilerExtension
 	public function loadConfiguration()
 	{
 		$builder = $this->getContainerBuilder();
-		
+
 		$config = $this->getConfig($this->defaults);
-		
+
 		$builder->addDefinition($this->prefix('client'))
-			->setClass('\Everyman\Neo4j\Client')
-			->setFactory('Bazo\Extensions\Neo4j\DI\Neo4jExtension::createNeo4jClient', array('@container', $config))
-			->setAutowired(FALSE);
-		
+				->setClass('\Everyman\Neo4j\Client')
+				->setFactory('Bazo\Extensions\Neo4j\DI\Neo4jExtension::createNeo4jClient', array('@container', $config))
+				->setAutowired(FALSE);
+
 		$builder->addDefinition($this->prefix('entityManager'))
-			->setClass('\HireVoice\Neo4j\EntityManager')
-			->setFactory('Bazo\Extensions\Neo4j\DI\Neo4jExtension::createEntityManager', array('@container', $config))
-			->setAutowired(FALSE);
+				->setClass('\HireVoice\Neo4j\EntityManager')
+				->setFactory('Bazo\Extensions\Neo4j\DI\Neo4jExtension::createEntityManager', array('@container', $config))
+				->setAutowired(FALSE);
 
 		$builder->addDefinition('entityManager')
-			->setClass('\HireVoice\Neo4j\EntityManager')
-			->setFactory('@container::getService', array($this->prefix('entityManager')));
-		
+				->setClass('\HireVoice\Neo4j\EntityManager')
+				->setFactory('@container::getService', array($this->prefix('entityManager')));
+
 		$builder->addDefinition($this->prefix('panel'))
-			->setFactory('\Bazo\Extensions\Neo4j\Diagnostics\Panel::register');
+				->setFactory('\Bazo\Extensions\Neo4j\Diagnostics\Panel::register');
 	}
-	
+
 	public static function createNeo4jClient(\Nette\DI\Container $container, $config)
 	{
 		return $container->neo4j->entityManager->getClient();
 	}
-	
+
 	public static function createEntityManager(\Nette\DI\Container $container, $config)
 	{
 		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(VENDOR_DIR . '/hirevoice/neo4jphp-ogm/lib/HireVoice/Neo4j/Annotation/Auto.php');
@@ -79,19 +77,17 @@ class Neo4jExtension extends \Nette\Config\CompilerExtension
 		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(VENDOR_DIR . '/hirevoice/neo4jphp-ogm/lib/HireVoice/Neo4j/Annotation/ManyToMany.php');
 		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(VENDOR_DIR . '/hirevoice/neo4jphp-ogm/lib/HireVoice/Neo4j/Annotation/ManyToOne.php');
 		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(VENDOR_DIR . '/hirevoice/neo4jphp-ogm/lib/HireVoice/Neo4j/Annotation/Property.php');
-		
+
 		$metadataCacheClass = self::$cacheClassMap[$config['metaDataCache']];
 		$metadataCache = new $metadataCacheClass;
 		$metadataCache->setNamespace($config['cachePrefix']);
-		
+
 		$reader = new \Doctrine\Common\Annotations\CachedReader(
-			new AnnotationReader,
-			$metadataCache,
-			false
+				new AnnotationReader, $metadataCache, false
 		);
-		
+
 		$configuration = new \HireVoice\Neo4j\Configuration(array(
-			'host' => $config['host'], 
+			'host' => $config['host'],
 			'port' => $config['port'],
 			'proxyDir' => $config['proxyDir'],
 			'debug' => $config['debug'],
@@ -99,16 +95,15 @@ class Neo4jExtension extends \Nette\Config\CompilerExtension
 			'password' => $config['password'],
 			'annotationReader' => $reader
 		));
-		
+
 		$em = new \HireVoice\Neo4j\EntityManager($configuration);
-		
+
 		$panel = $container->neo4j->panel;
-		$em->registerEvent(\HireVoice\Neo4j\EntityManager::QUERY_RUN, function($query, $parameters, $time)use($panel){
-			$panel->addQuery($query, $parameters, $time);
-		});
-		
+		$em->registerEvent(\HireVoice\Neo4j\EntityManager::QUERY_RUN, function($query, $parameters, $time)use($panel) {
+					$panel->addQuery($query, $parameters, $time);
+				});
+
 		return $em;
 	}
-	
-	
+
 }
--- Bazo/Extensions/Neo4j/Diagnostics/Panel.php
+++ Bazo/Extensions/Neo4j/Diagnostics/Panel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (c) 2012 Martin Bazik <martin@bazo.sk>
  *
@@ -9,7 +10,6 @@ namespace Bazo\Extensions\Neo4j\Diagnostics;
 
 use Everyman\Neo4j\Query;
 use HireVoice\Neo4j;
-
 use Nette;
 use Nette\Diagnostics\Debugger;
 
@@ -36,7 +36,8 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 
 	private function getQueryType(Query $query = null)
 	{
-		if($query === null) return null;
+		if ($query === null)
+			return null;
 		$types = array(
 			'Everyman\Neo4j\Cypher\Query' => 'cypher',
 			'Everyman\Neo4j\Gremlin\Query' => 'gremlin'
@@ -48,14 +49,14 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 	public function addQuery(Query $query = null, $parameters = array(), $time = null)
 	{
 		$type = $this->getQueryType($query);
-		$queryEntry = (object)array(
-			'type' => $type,
-			'query'=> ($query !== null) ? $query->getQuery() : '',
-			'parameters' => $parameters,
-			'time' => $time,
-			'results' => ($query !== null) ? $query->getResultSet()->count() : 'n/a'
+		$queryEntry = (object) array(
+					'type' => $type,
+					'query' => ($query !== null) ? $query->getQuery() : '',
+					'parameters' => $parameters,
+					'time' => $time,
+					'results' => ($query !== null) ? $query->getResultSet()->count() : 'n/a'
 		);
-		
+
 		$this->totalTime += $time;
 		$this->queries[] = $queryEntry;
 	}
@@ -67,10 +68,10 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 	public function getTab()
 	{
 		return '<span title="Neo4j">' .
-			'<img alt="" src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/2wBDAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/wAARCAAQABADASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAACAYH/8QAHxAAAgIDAQEAAwAAAAAAAAAABAUDBgECBwgJEhMU/8QAFQEBAQAAAAAAAAAAAAAAAAAABwn/xAAdEQADAAMBAQEBAAAAAAAAAAADBAUBAgYTEgcR/9oADAMBAAIRAxEAPwCP5DdOE1vy5cq3buF0y99UtPQq7HVLy+KKFlRLBxUDCUBjMDquarkO2F7eAvau2IBg4w8IiP2AiXhmStP2r8wecefvOLrtlUu5k9gr0yTaw182EHarMYrI1DTYFpO0ukj8TCwpnCSLh05sJRqoUmWciMjX884B846d5m6cd0qk+l+gp67Wcoau+r1ae3IGkp3btXlyMU71fElgSQOawAftEIGGwFnNAdtckxGLxS4cHr0r2aawlH8iqvULrd+N8zd2hPQyLRfnVlEsCsBu2Erb6FYXKMmXwj1zYJalhAUwSQBfu3/o20NwMMltAuP/AKSOTydzoIAIVpTpu5FSnuUYnToV0pwl50Wg+coU/AM86xk0BrrhYdK4L0ZSIMc6B7Iig+zyyLO7SeykzIyCA4k0swXJTHGuPJD4JhgZsEb31+tQ+P8APncW5P/Z" />' .
-			count($this->queries) . ' queries' .
-			($this->queries ? ' / ' . sprintf('%0.1f', $this->totalTime * 1000) . ' ms' : '') .
-			'</span>';
+				'<img alt="" src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/2wBDAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/wAARCAAQABADASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAACAYH/8QAHxAAAgIDAQEAAwAAAAAAAAAABAUDBgECBwgJEhMU/8QAFQEBAQAAAAAAAAAAAAAAAAAABwn/xAAdEQADAAMBAQEBAAAAAAAAAAADBAUBAgYTEgcR/9oADAMBAAIRAxEAPwCP5DdOE1vy5cq3buF0y99UtPQq7HVLy+KKFlRLBxUDCUBjMDquarkO2F7eAvau2IBg4w8IiP2AiXhmStP2r8wecefvOLrtlUu5k9gr0yTaw182EHarMYrI1DTYFpO0ukj8TCwpnCSLh05sJRqoUmWciMjX884B846d5m6cd0qk+l+gp67Wcoau+r1ae3IGkp3btXlyMU71fElgSQOawAftEIGGwFnNAdtckxGLxS4cHr0r2aawlH8iqvULrd+N8zd2hPQyLRfnVlEsCsBu2Erb6FYXKMmXwj1zYJalhAUwSQBfu3/o20NwMMltAuP/AKSOTydzoIAIVpTpu5FSnuUYnToV0pwl50Wg+coU/AM86xk0BrrhYdK4L0ZSIMc6B7Iig+zyyLO7SeykzIyCA4k0swXJTHGuPJD4JhgZsEb31+tQ+P8APncW5P/Z" />' .
+				count($this->queries) . ' queries' .
+				($this->queries ? ' / ' . sprintf('%0.1f', $this->totalTime * 1000) . ' ms' : '') .
+				'</span>';
 	}
 
 	/**
@@ -82,22 +83,21 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 		$s = '';
 		$h = 'htmlSpecialChars';
 		foreach ($this->queries as $query) {
-			$s .= '<tr><td>' . sprintf('%0.3f', $query->time * 1000000).'</td>';
+			$s .= '<tr><td>' . sprintf('%0.3f', $query->time * 1000000) . '</td>';
 			$s .= '<td class="neo4j-query">' .
-				$h(self::$maxLength ? Nette\Utils\Strings::truncate($query->query, self::$maxLength) : $query->query)
-				.'</td>';
+					$h(self::$maxLength ? Nette\Utils\Strings::truncate($query->query, self::$maxLength) : $query->query)
+					. '</td>';
 			$parameters = '';
-			foreach($query->parameters as $key => $value)
-			{
-				$parameters .= $key.': '.$value."\n";
+			foreach ($query->parameters as $key => $value) {
+				$parameters .= $key . ': ' . $value . "\n";
 			}
-			$s .= '<td>'.$parameters.'</td>';
-			$s .= '<td>'.$query->results.'</td>';
+			$s .= '<td>' . $parameters . '</td>';
+			$s .= '<td>' . $query->results . '</td>';
 			$s .= '</tr>';
 		}
 
 		return empty($this->queries) ? '' :
-			'<style> #nette-debug div.neo4j-panel table td { text-align: right }
+				'<style> #nette-debug div.neo4j-panel table td { text-align: right }
 			#nette-debug div.neo4j-panel table td.neo4j-query { background: white !important; text-align: left } </style>
 			<h1>Queries: ' . count($this->queries) . ($this->totalTime ? ', time: ' . sprintf('%0.3f', $this->totalTime * 1000) . ' ms' : '') . '</h1>
 			<div class="nette-inner neo4j-panel">
@@ -117,11 +117,11 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 			$panel = NULL;
 			if ($e->getQuery() !== null) {
 				$panel .= '<h3>Query</h3>'
-					. '<pre class="nette-dump"><span class="php-string">'
-					. $e->getQuery()->getQuery()
-					. '</span></pre>';
+						. '<pre class="nette-dump"><span class="php-string">'
+						. $e->getQuery()->getQuery()
+						. '</span></pre>';
 			}
-			
+
 			if ($panel !== NULL) {
 				$panel = array(
 					'tab' => 'Neo4j',
@@ -132,7 +132,7 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 			return $panel;
 		}
 	}
-	
+
 	/**
 	 * @return \Bazo\Extensions\Neo4j\Diagnostics\Panel
 	 */
@@ -143,4 +143,5 @@ class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 		Debugger::$bar->addPanel($panel);
 		return $panel;
 	}
+
 }
